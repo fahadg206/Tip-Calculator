39940909f9cfe76bf674dea4cb2f9e75c644ef8c
Fahad Guled <fahad.guled@yahoo.com>
2022-02-08 19:13:42 -0800

    changed the formatting of tip percentage. I made the tip a whole number instead of a decimal then divided the tip percentage by 100.

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ let tip = 0;
 
 // Tip Counter //
 increaseTip.addEventListener('click', function(){
-    tip += 1/100;
-    tipPercentage.innerHTML = tip;
+    tip += 1;
+    tipPercentage.innerHTML =  tip + "%" ;
    
 });
 
 decreaseTip.addEventListener('click', function() {
-    tip -= 1/100
-    tipPercentage.innerHTML = tip;
+    tip -= 1;
+    tipPercentage.innerHTML = tip + "%" ;
 })
 
 
@@ -28,15 +28,15 @@ const costOfMeal = Number(customerText);
 
 const tipAmount = document.getElementById('demo');
 // Customer is able to see how much they are tipping //
-tipAmount.innerHTML = "Tip: " + "$" + costOfMeal * tip
+tipAmount.innerHTML = "Tip: " + "$" + costOfMeal *tip / 100
 
-const tipTotall = costOfMeal * tip;
+const tipTotall = costOfMeal * tip / 100;
 const tipTotal = Number(tipTotall);
 console.log(typeof(tipTotal))
 const finalTotal = document.getElementById('finalTotal');
 
 // Customer is able to see total after tip and cost of meal is added //
-finalTotal.innerHTML = `Total:  $  ${costOfMeal + tipTotal}`;
+finalTotal.innerHTML = `Total:  $${costOfMeal + tipTotal}`;
 
 console.log(typeof(costOfMeal));
 console.log(typeof(tipTotal));
